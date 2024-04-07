ce06fc8636db35ba71298aeb13ab038ad3aa8b5d
house <piyushkumarsometing@gmail.com>
2024-04-07 07:44:25 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("some-route")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("some-route")]
+        public IActionResult Other()
+        {
+            return View();
+        }
+    }
+}
